154d55c8b74e985bce52fab5a747fc95486de739
Guillaume Clement <gclement@baobob.org>
2014-08-20 23:18:32 +0200

    framebuffer: Introduce static inline methods for color conversions
    Static inline methods allow typesafe functions while keeping the zero
    overhead of macros.
    
    Signed-off-by: Guillaume Clement <gclement@baobob.org>

--- graphics/framebuffer.h
+++ graphics/framebuffer.h
@@ -40,5 +40,20 @@ void fb_printf(struct framebuffer *fb, unsigned int x, unsigned int y, uint32_t 
 #define C_16_TO_RGB(c,r,g,b) do { r = ((c>>11)&31)*8; g = ((c>>5)&63)*4; b = (c&31)*8; } while(0)
 #define C_32_TO_RGB(c,r,g,b) do { r = (c>>16)&0xff; g = (c>>8)&0xff; b = c&0xff; } while(0)
 
+static inline uint16_t rgb_to_16(uint8_t r, uint8_t g, uint8_t b)
+{
+	return ((r / 8) << 11) | ((g / 4) << 5) | (b / 8);
+}
+
+static inline uint32_t rgb_to_24(uint8_t r, uint8_t g, uint8_t b)
+{
+	return (r << 16) | (g << 8) | b;
+}
+
+static inline uint16_t c24_to_16(uint32_t rgb)
+{
+	return rgb_to_16(rgb >> 16, rgb >> 8, rgb);
+}
+
 
 #endif
